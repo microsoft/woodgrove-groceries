6c183d58006b215bd76af9acad04c4106909cecc
Yoel Horvitz <yoelhor@live.com>
2024-02-28 17:17:26 +0200

    Token augmentation help

---
+++ Areas/Help/Pages/TokenAugmentation.cshtml.cs
@@ -0,0 +1,25 @@
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace woodgrovedemo.Help.Pages
+{
+    public class TokenAugmentationModel : PageModel
+    {
+        private TelemetryClient _telemetry;
+        public TokenAugmentationModel(TelemetryClient telemetry)
+        {
+            _telemetry = telemetry;
+        }
+
+        public void OnGet()
+        {
+            PageViewTelemetry pageView = new PageViewTelemetry("TokenAugmentation");
+
+            // Type of the page
+            pageView.Properties.Add("Area", "Help");
+            _telemetry.TrackPageView(pageView);
+        }
+    }
+}
