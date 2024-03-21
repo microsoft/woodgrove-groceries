667eca058171087dd92834add089e84e99c0e945
Yoel Horvitz <yoelhor@live.com>
2024-03-21 17:30:01 +0200

    Graph API create uf, app, and IDP

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,20 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+function showGraphAPI(){
+    $("#stepNavigatorContainer").css('visibility', 'hidden');
+    $("#showGraphAPI").hide();
+    $(".bs-stepper").hide();
+    $("#showEntraAdminCenter").show();
+    $("#GraphApiContent").show();
+    
+
+}
+
+function showEntraAdminCenter(){
+    $("#stepNavigatorContainer").css('visibility', 'visible');;
+    $("#showGraphAPI").show();
+    $(".bs-stepper").show();
+    $("#showEntraAdminCenter").hide();
+    $("#GraphApiContent").hide();
+}
