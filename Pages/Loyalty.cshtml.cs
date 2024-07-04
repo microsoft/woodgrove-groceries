3f7f98f6fc5644f704c773243e90ed86ffcb845a
Yoel Horvitz <yoelhor@live.com>
2024-07-04 16:57:59 +0300

    Loyalty program UI

---
+++ Pages/Loyalty.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace woodgrovedemo.Pages
+{
+    public class LoyaltyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
