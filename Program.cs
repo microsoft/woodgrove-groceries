84b9dd16794b947042568b0b6868c19b8fed6b97
Yoel Horvitz <yoelhor@live.com>
2024-10-30 17:35:26 +0200

    Full code

--- Program.cs
+++ Program.cs
@@ -37,30 +37,29 @@ builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
 // Invite sign-in flow
 ConfigurationSection Invite = (ConfigurationSection)builder.Configuration.GetSection("Invite");
 builder.Services.AddAuthentication("Invite")
-    .AddMicrosoftIdentityWebApp(Invite, "Invite", "cookiesInvite");
-// .EnableTokenAcquisitionToCallDownstreamApi()
-// .AddDownstreamApi("WoodgroveGroceriesApiInvite", WoodgroveGroceriesApi)
-// .AddMicrosoftGraph(builder.Configuration.GetSection("GraphApi"))
-// .AddInMemoryTokenCaches();
+    .AddMicrosoftIdentityWebApp(Invite, "Invite", "cookiesInvite")
+    .EnableTokenAcquisitionToCallDownstreamApi()
+    .AddDownstreamApi("WoodgroveGroceriesApiInvite", WoodgroveGroceriesApi)
+    .AddMicrosoftGraph(builder.Configuration.GetSection("GraphApi"))
+    .AddInMemoryTokenCaches();
 
 
 // Set authentication options for all schemes
-// foreach (var scheme in allSchemes)
-// {
-builder.Services.Configure<OpenIdConnectOptions>(OpenIdConnectDefaults.AuthenticationScheme,
-          options =>
-          {
-              options.TokenValidationParameters.RoleClaimType = "roles";
-              options.TokenValidationParameters.NameClaimType = "name";
-              options.Events.OnRedirectToIdentityProvider += OnRedirectToIdentityProviderFunc;
-              options.Events.OnMessageReceived += OnMessageReceivedFunc;
-              options.Events.OnAuthenticationFailed += OnAuthenticationFailedFunc;
-              options.Events.OnRemoteFailure += OnRemoteFailureFunc;
-              options.RemoteAuthenticationTimeout = TimeSpan.FromMinutes(30);
-              options.SaveTokens = true;
-          });
-//}
-
+foreach (var scheme in allSchemes)
+{
+    builder.Services.Configure<OpenIdConnectOptions>(OpenIdConnectDefaults.AuthenticationScheme,
+              options =>
+              {
+                  options.TokenValidationParameters.RoleClaimType = "roles";
+                  options.TokenValidationParameters.NameClaimType = "name";
+                  options.Events.OnRedirectToIdentityProvider += OnRedirectToIdentityProviderFunc;
+                  options.Events.OnMessageReceived += OnMessageReceivedFunc;
+                  options.Events.OnAuthenticationFailed += OnAuthenticationFailedFunc;
+                  options.Events.OnRemoteFailure += OnRemoteFailureFunc;
+                  options.RemoteAuthenticationTimeout = TimeSpan.FromMinutes(30);
+                  options.SaveTokens = true;
+              });
+}
 
 builder.Services.AddAuthorization(options =>
 {
